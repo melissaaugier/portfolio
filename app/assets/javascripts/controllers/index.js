06bb30ad703223c63e95516946116fe4dee1c6f6
Mélissa Augier <augiermelissa@gmail.com>
2025-02-27 17:00:34 +0100

    slide in slide out

--- app/assets/javascripts/controllers/index.js
+++ app/assets/javascripts/controllers/index.js
@@ -1,9 +1,7 @@
 //= require stimulus
 //= require ./hello_controller
-//= require ./modal_controller
 
 const application = Stimulus.Application.start();
 
 // Manually register each controller
 application.register("hello", HelloController);
-application.register("modal", ModalController);
