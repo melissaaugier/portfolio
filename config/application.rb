6d7cae535b0e54c65048c30f6cdd5d572f0bbcc9
Mélissa Augier <augiermelissa@gmail.com>
2025-03-07 15:32:37 +0100

    Add SSL redirect

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Portfolio
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-    config.middleware.insert_before(Rack::Runtime, Rack::SSL)
+    config.force_ssl = true
 
     # Configuration for the application, engines, and railties goes here.
     #
