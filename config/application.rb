4617e46da3884e12bd38242794d3e3a21cc47c38
Mélissa Augier <augiermelissa@gmail.com>
2025-03-07 15:49:25 +0100

    force ssl

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Portfolio
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
     config.force_ssl = true
+    config.ssl_options = { hsts: { subdomains: true } }
 
     # Configuration for the application, engines, and railties goes here.
     #
