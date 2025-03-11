316a036d6ede385a2c50d04436ba6d986986ce50
Mélissa Augier <augiermelissa@gmail.com>
2025-03-11 17:11:02 +0100

    rollback

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,13 +25,9 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
-  config.assets.compile = false
-  config.assets.digest = true
-  config.assets.js_compressor = :uglifier # ou :terser si vous avez une version plus récente
-  config.assets.css_compressor = :scss
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
-  # config.assets.compile = true
+  config.assets.compile = true
   config.assets.initialize_on_precompile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
